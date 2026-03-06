668e8ce4dfcd5bb88c0ab83a292f1d00db00d4a2
Johnathan Baggett <jbagg1986@gmail.com>
2026-03-06 14:59:13 -0500

    Add project files.

---
+++ Movie/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+using RazorPagesMovie.Utils;
+using Microsoft.AspNetCore.Hosting;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public CreateModel(RazorPagesMovie.Data.RazorPagesMovieContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public RazorPagesMovie.Models.Movie Movie { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // handle the uploaded image file if present
+            if (HttpContext.Request.Form.Files.Count > 0)
+            {
+                // store path in Movie.ImageUri using the injected environment instance (_env)
+                Movie.ImageUri = PictureHelper.UploadNewImage(_env,
+                    HttpContext.Request.Form.Files[0]);
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
