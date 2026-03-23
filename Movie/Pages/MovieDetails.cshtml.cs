46c6c8b82beeaac1dbfb386a38f1f15d19fde2a7
Johnathan Baggett <jbagg1986@gmail.com>
2026-03-23 12:31:15 -0400

    final

---
+++ Movie/Pages/MovieDetails.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorPagesMovieContext _context;
+
+        public DetailsModel(RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public RazorPagesMovie.Models.Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            Movie = movie;
+            return Page();
+        }
+    }
+}
